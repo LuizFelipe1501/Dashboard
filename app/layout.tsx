03fdae592a88aec46c2e6f2e8ce3f20b8d7b7990
My-Ticket-tech <202798304+LuizFelipe1501@users.noreply.github.com>
2026-01-09 03:36:54 -0300

    11.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="bg-black text-white">{children}</body>
+    <html lang="en" className="h-full">
+      <body className="
+        h-full 
+        bg-[#091b45] text-white 
+        overflow-x-hidden 
+        font-sans antialiased
+      ">
+        {children}
+      </body>
     </html>
   );
 }
